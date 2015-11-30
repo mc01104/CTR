c3ff6f1b50545f564618ac98d3205059d2eb9988
george <georgios.fagogenis@gmail.com>
2015-11-30 14:09:32 -0500

    with meta rate

--- CTR/src/LWPRKinematics.cpp
+++ CTR/src/LWPRKinematics.cpp
@@ -24,9 +24,9 @@ LWPRKinematics::LWPRKinematics(const ::std::string& pathToForwardModel):
 	//metaData.open(name);
 
 	forwardModel->updateD(true);
-	//forwardModel->useMeta(true);
-	//forwardModel->metaRate(0.1);
-	forwardModel->setInitAlpha(0.01);
+	forwardModel->useMeta(true);
+	forwardModel->metaRate(0.1);
+	forwardModel->setInitAlpha(0.001);
 	forwardModel->wPrune(1.0);
 	forwardModel->initLambda(0.995);
 	forwardModel->finalLambda(0.995);

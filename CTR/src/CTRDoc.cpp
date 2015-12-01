6a58e4b00247743901b768bdd141017983be8f94
george <georgios.fagogenis@gmail.com>
2015-12-01 12:28:12 -0500

    sensor + adaptation

--- CTR/src/CTRDoc.cpp
+++ CTR/src/CTRDoc.cpp
@@ -943,10 +943,10 @@ unsigned int WINAPI	CCTRDoc::MotorLoop(void* para)
 	double dCnt[7];		
 	
 	//double K[6] = {3.0, 3.0, 3.0, 6.0, 6.0, 6.0 };	// working
-	double K[6] = {10.0, 10.0, 10.0, 10.0, 10.0, 10.0 };		// working
+	//double K[6] = {10.0, 10.0, 10.0, 10.0, 10.0, 10.0 };		// working
 	//double K[6] = {20.0, 20.0, 20.0, 20.0, 20.0, 20.0 };		// working
 	//double K[6] = {10.0, 10.0, 10.0, 10.0, 10.0, 10.0 };		// working
-	//double K[6] = { 5.0, 5.0, 5.0, 5.0, 5.0, 5.0 };				// For sensor feedback + estimator
+	double K[6] = { 5.0, 5.0, 5.0, 5.0, 5.0, 5.0 };				// For sensor feedback + estimator
 	//double K[6] = { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };				// For sensor feedback + estimator
 		
 

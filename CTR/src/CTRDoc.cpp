afd730f67e68eee950ca70db521d8c87405f54bc
George <georgios.fagogenis@gmail.com>
2017-08-21 15:40:12 -0400

    activate contact control with apex-to-valve

--- CTR/src/CTRDoc.cpp
+++ CTR/src/CTRDoc.cpp
@@ -2922,8 +2922,10 @@ void CCTRDoc::ToggleCircumnavigation()
 void CCTRDoc::ToggleApexToValve()
 {
 
+
 	this->m_apex_to_valve = !this->m_apex_to_valve;
-	
+
+
 	::std::cout << "apex-to-valve navigation: ";
 	(this->m_apex_to_valve ?	::std::cout << "ON" : ::std::cout << "OFF");
 	::std::cout << ::std::endl;

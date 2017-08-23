c69b00c504c45e14fe7b9bd797a594e5a5a4357f
george <georgios.fagogenis@gmail.com>
2017-08-23 11:13:41 -0400

    small GUI bug regarding contact control fixed

--- CTR/src/CTRDoc.cpp
+++ CTR/src/CTRDoc.cpp
@@ -2763,7 +2763,7 @@ bool CCTRDoc::TeleOpSafetyCheck()
 void CCTRDoc::ToggleForceControl()
 {
 	this->m_forceControlActivated = !this->m_forceControlActivated;
-	::std::cout << "contact controller is" << (this->m_forceControlActivated ? "ON" : "OFF") << ::std::endl;
+	::std::cout << "contact controller is " << (this->m_forceControlActivated ? "ON" : "OFF") << ::std::endl;
 
 }
 
@@ -2957,7 +2957,10 @@ void CCTRDoc::ToggleApexToValve()
 	this->m_apex_to_valve = !this->m_apex_to_valve;
 
 	if (this->m_apex_to_valve)
+	{
 		this->m_circumnavigation = false;
+		this->m_forceControlActivated = false;
+	}
 
 	::std::cout << "apex-to-valve navigation: ";
 	(this->m_apex_to_valve ?	::std::cout << "ON" : ::std::cout << "OFF");

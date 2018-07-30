5765fd3a82b27d17effa36dfd2ec4a6291983b77
george <georgios.fagogenis@gmail.com>
2018-07-30 14:47:27 -0400

    latest

--- ReadHeartRate/HeartRateMonitor.cpp
+++ ReadHeartRate/HeartRateMonitor.cpp
@@ -63,15 +63,15 @@ void HeartRateMonitor::run()
 		if (source.size() <= 0)
 			continue;
 		
-		if(!checkForConsistency(strings[index[1]]))
-			continue;
-		else
-		{
+		//if(!checkForConsistency(strings[index[1]]))
+		//	continue;
+		//else
+		//{
 			heartRate = atof(strings[this->source].c_str());
 			breathingRate = atof(strings[strings.size()-1].c_str());
-			//::std::cout << " Heart rate [bpm]:" << heartRate << ::std::endl;
+			::std::cout << " Heart rate [bpm]:" << heartRate << ::std::endl;
 			//::std::cout << " Breathing rate [bpm]:" << breathingRate << ::std::endl;
-		}
+		//}
 
 
 		counter++;
